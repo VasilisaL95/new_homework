98fe75df7ec8fe5e498c4b3dd81fb60795da00bc
VasilisaL95 <silisochka@mail.ru>
2024-11-22 23:51:12 +0300

    22/11

---
+++ module_4_2.py
@@ -0,0 +1,13 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+
+#    inner_function() - ничего не возвращает
+
+#inner_function() - ошибка
+
+#test_function() - работает
+
+
+
