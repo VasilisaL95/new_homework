f02015605a9b073c6fffd16bfb415bccb6787879
VasilisaL95 <silisochka@mail.ru>
2024-12-15 17:35:09 +0300

    15/12

---
+++ mod_8/module_8_3.py
@@ -0,0 +1,61 @@
+class Car: #должен обладать следующими свойствами:
+    def __init__(self, model, __vin, __numbers):
+        self.model = model          #- название автомобиля (строка).
+        self.__vin = __vin          #- vin номер автомобиля (целое число). Уровень доступа private.
+        self.__numbers = __numbers  # - номера автомобиля (строка).
+        self.__is_valid_vin(self.__vin)
+        self.__is_valid_numbers(self.__numbers)
+
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        if vin_number < 1000000 or vin_number > 9999999:
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        return True
+
+    def __is_valid_numbers(self, numbers):    #- принимает numbers и проверяет его на корректность.
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        if len(numbers) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        return True
+
+
+class IncorrectVinNumber(Exception):
+    def __init__(self, massage):
+        self.message = massage
+
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, massage):
+        self.message = massage
+
+
+try:
+    first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+
+
+try:
+    second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+
+
+try:
+    third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
